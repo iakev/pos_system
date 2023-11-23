94d95a9887c0d3737b827af6cb8d1a4770fbd588
iakev <kirimikmwongera@gmail.com>
2023-11-23 23:00:15 +0000

    Added a context provider to work as the statemanagement for the application absolute minimums like user and authentication token

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import APIProvider from "./services/api/APIProvider";
 import Home from "./pages/home/Home";
 import List from "./pages/list/List";
 import Single from "./pages/single/Single";
@@ -15,34 +16,36 @@ import SingleSupplier from "./pages/suppliers/SingleSupplier";
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/employees" element={<List />} />
-          <Route path="/employees/:id" element={<SingleEmployee />} />
-          <Route
-            path="/employees/new"
-            element={<New inputs={userInputs} title="Add New Employee" />}
-          />
-          <Route
-            path="users/new"
-            element={<New inputs={userInputs} title="Add New User" />}
-          />
-          <Route path="/products" element={<Products />} />
-          <Route path="/products/:id" element={<SingleProduct />} />
-          <Route
-            path="products/new"
-            element={<New inputs={productInputs} title="Add New Product" />}
-          />
-          <Route path="/business" element={<Business />} />
-          <Route path="/suppliers" element={<Suppliers />} />
-          <Route path="/suppliers/:id" element={<SingleSupplier />} />
-          <Route
-            path="/suppliers/new"
-            element={<New inputs={supplierInputs} title="Add New Supplier" />}
-          />
-        </Routes>
-      </BrowserRouter>
+      <APIProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/employees" element={<List />} />
+            <Route path="/employees/:id" element={<SingleEmployee />} />
+            <Route
+              path="/employees/new"
+              element={<New inputs={userInputs} title="Add New Employee" />}
+            />
+            <Route
+              path="users/new"
+              element={<New inputs={userInputs} title="Add New User" />}
+            />
+            <Route path="/products" element={<Products />} />
+            <Route path="/products/:id" element={<SingleProduct />} />
+            <Route
+              path="products/new"
+              element={<New inputs={productInputs} title="Add New Product" />}
+            />
+            <Route path="/business" element={<Business />} />
+            <Route path="/suppliers" element={<Suppliers />} />
+            <Route path="/suppliers/:id" element={<SingleSupplier />} />
+            <Route
+              path="/suppliers/new"
+              element={<New inputs={supplierInputs} title="Add New Supplier" />}
+            />
+          </Routes>
+        </Router>
+      </APIProvider>
     </div>
   );
 }
