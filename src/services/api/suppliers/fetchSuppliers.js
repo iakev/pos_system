d96425c67f3421af72618fa377b6187946b24fbf
iakev <kirimikmwongera@gmail.com>
2023-12-18 09:50:43 +0000

    Added toast error notifications on failure to make requests

--- src/services/api/suppliers/fetchSuppliers.js
+++ src/services/api/suppliers/fetchSuppliers.js
@@ -1,40 +1,41 @@
 import makeRequest from "../makeRequests";
+import { toast } from "react-toastify";
 
 const fetchSuppliers = async function fetchSuppliersService(context) {
-  const uri = "/api/v1/suppliers/";
+  const uri = "suppliers/";
   const method = "GET";
   let suppliers;
   try {
     suppliers = await makeRequest(context, uri, { method });
   } catch (error) {
-    const getSuppliersError = { message: ("There is a problem in getting Suppliers:", error) }
-    throw getSuppliersError;
+    const getSuppliersError = { message: ("There is a problem in getting Suppliers:", error) };
+    toast.error(getSuppliersError, { autoClose: 5000 });
   }
   return suppliers;
 }
 
 const fetchSingleSupplier = async function fetchSingleSupplierService(context, uuid) {
-  const uri = `/api/v1/suppliers/${uuid}`;
+  const uri = `suppliers/${uuid}`;
   const method = "GET";
   let supplier;
   try {
     supplier = await makeRequest(context, uri, { method });
   } catch (error) {
-    const getSuppliersError = { message: ("There is a problem in getting Supplier:", error) }
-    throw getSuppliersError;
+    const getSupplierError = { message: ("There is a problem in getting Supplier:", error) };
+    toast.error(getSupplierError, { autoClose: 5000 });
   }
   return supplier;
 }
 
 const fetchAllProductsForSupplier = async function fetchAllProductsSupplier(context, supplier_uuid) {
-  const uri = `/api/v1/suppliers/${supplier_uuid}/list_all_products/`;
+  const uri = `suppliers/${supplier_uuid}/list_all_products/`;
   const method = "GET";
   let products;
   try {
     products = await makeRequest(context, uri, { method });
   } catch (error) {
-    const getProductsError = { message: ("There is a problem in getting all Suppliers for Product:", error) }
-    throw getProductsError;
+    const getProductsError = { message: ("There is a problem in getting all Suppliers for Product:", error) };
+    toast.error(getProductsError, { autoClose: 5000 });
   }
   return products;
 }
