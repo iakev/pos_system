57d394e51f601f4fb77b617c92a78c8f8069cad8
iakev <kirimikmwongera@gmail.com>
2023-12-18 09:50:18 +0000

    Added toast error notifications on failure to make requests

--- src/services/api/stocks/fetchStocks.js
+++ src/services/api/stocks/fetchStocks.js
@@ -1,40 +1,41 @@
 import makeRequest from "../makeRequests";
+import { toast } from "react-toastify";
 
 const fetchStocks = async function fetchStocksService(context) {
-  const uri = "/api/v1/stocks/";
+  const uri = "stocks/";
   const method = "GET";
   let stocks;
   try {
     stocks = await makeRequest(context, uri, { method });
   } catch (error) {
     const getStocksError = { message: ("There is a problem in getting Stocks:", error) };
-    throw getStocksError;
+    toast.error(getStocksError, { autoClose: 5000 });
   }
   return stocks;
 };
 
 const fetchSingleStock = async function fetchSingleStockService(context, uuid) {
-  const uri = `/api/v1/stocks/${uuid}`;
+  const uri = `stocks/${uuid}`;
   const method = "GET";
   let stock;
   try {
     stock = await makeRequest(context, uri, { method });
   } catch (error) {
     const getStockError = { message: ("There is a problem in getting Stock:", error) };
-    throw getStockError;
+    toast.error(getStockError, { autoClose: 5000 });
   }
   return stock;
 };
 
 const fetchSingleStockMovement = async function fetchSingleStockMovementService(context, uuid) {
-  const uri = `/api/v1/stocks/${uuid}/list_all_stock_movements/`;
+  const uri = `stocks/${uuid}/list_all_stock_movements/`;
   const method = "GET";
   let stockMovement;
   try {
     stockMovement = await makeRequest(context, uri, { method });
   } catch (error) {
     const getStockMovementError = { mesage: ("There is a problem in getting the stock movement:", error) };
-    throw getStockMovementError;
+    toast.error(getStockgetStockMovementErrorError, { autoClose: 5000 });
   }
   return stockMovement;
 }
