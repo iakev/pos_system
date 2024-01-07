5b238e3aa784596a3ac77dac003ac66914b1f01a
iakev <kirimikmwongera@gmail.com>
2024-01-07 15:36:44 +0000

    added settings for the theme currently in use

---
+++ src/theme/theme.js
@@ -0,0 +1,24 @@
+const theme = {
+  palette: {
+    primary: {
+      main: '#7451f8',
+    },
+    secondary: {
+      main: '#d4f851',
+    },
+    info: {
+      main: '#5180f8',
+    },
+    success: {
+      main: '#c951f8',
+    },
+    warning: {
+      main: '#f851d4',
+    },
+    error: {
+      main: '#f87551',
+    },
+  },
+};
+
+export default theme;
