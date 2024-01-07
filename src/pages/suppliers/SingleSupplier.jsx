a9a4ce03d56f6d87cca2a22f4f6136012e6d24b5
iakev <kirimikmwongera@gmail.com>
2024-01-07 15:31:06 +0000

    Modified supplier detail page to conform to components design

--- src/pages/suppliers/SingleSupplier.jsx
+++ src/pages/suppliers/SingleSupplier.jsx
@@ -1,76 +1,53 @@
+import React, { useEffect, useState } from "react";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { TOKEN } from "../../../../token";
+import { useAPI } from "../../services/api/useAPI";
+import { fetchSingleSupplier } from "../../services/api/suppliers/fetchSuppliers";
+import SupplierInfo from "../../components/supplierComponents/supplierInfo/SupplierInfo";
+import SupplierProducts from "../../components/supplierComponents/supplierProducts/SupplierProducts";
+import ContentGrid from "../../components/pageContent/ContentGrid";
 
 const SingleSupplier = () => {
   const [supplier, setSupplier] = useState({});
   const { id } = useParams();
+  const apiContext = useAPI();
 
   useEffect(() => {
-    const fetchProduct = async () => {
-      const url = `http://127.0.0.1:8000/api/v1/suppliers/${id}`;
-      const headers = {
-        Authorization: `Token  ${TOKEN}`,
-        "Content-Type": "application/json",
-      };
-
-      const response = await fetch(url, {
-        method: "GET",
-        headers: headers,
-      });
-
-      const result = await response.json();
-
-      setSupplier(result);
+    const getSupplier = async () => {
+      const singleSupplier = await fetchSingleSupplier(apiContext, id)
+      setSupplier(singleSupplier);
     };
-
-    fetchProduct();
+    getSupplier();
   }, []);
 
-  const { name, address, email_address, phone_number, products } = supplier;
+  const singleSupplierPageContent = {
+    isLoading: Object.keys(supplier).length === 0,
+    sections: [
+      {
+        component: SupplierInfo,
+        componentProps: {
+          name: supplier?.name,
+          instance: supplier,
+        }
+      },
+      {
+        component: SupplierProducts,
+        componentProps: {
+          uuid: id,
+          context: apiContext,
+        }
+      },
+    ]
+  }
+
 
   return (
     <div className="single">
       <Sidebar />
       <div className="singleContainer">
         <Navbar />
-        {supplier ? (
-          <div className="top">
-            <div className="left">
-              <div className="editButton">Edit</div>
-              <h1 className="title">Information</h1>
-              <div className="item">
-                <div className="details">
-                  <h1 className="itemTitle">{name}</h1>
-                  <div className="detailItem">
-                    <span className="itemKey">Address:</span>
-                    <span className="itemValue">{address}</span>
-                  </div>
-                  <div className="detailItem">
-                    <span className="itemKey">Active for sale:</span>
-                    {products?.length ? (
-                      <span className="itemValue">True</span>
-                    ) : (
-                      <span className="itemValue">False</span>
-                    )}
-                  </div>
-                  <div className="detailItem">
-                    <span className="itemKey">Email Address:</span>
-                    <span className="itemValue">{email_address}</span>
-                  </div>
-                  <div className="detailItem">
-                    <span className="itemKey">Phone Number:</span>
-                    <span className="itemValue">{phone_number}</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        ) : (
-          <p>Loading...</p>
-        )}
+        <ContentGrid content={singleSupplierPageContent} />
       </div>
     </div>
   );
