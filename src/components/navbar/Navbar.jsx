0c9f5eaa8831fdc3c82c8b5ad1eb1e42c1078558
iakev <kirimikmwongera@gmail.com>
2024-01-07 15:17:08 +0000

    Modified navbar to use badges

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,13 @@
 import "./navbar.scss";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
-import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
-import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
-import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
 import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
-// import { DarkModeContext } from "../../context/darkModeContext";
-// import { useContext } from "react";
+import NotificationBadge from "../badges/NotificationsBadge";
+import ChatBadge from "../badges/ChatBadge";
+import DarkModeToggle from "../darkModeToggle/DarkModeToggle";
 
 const Navbar = () => {
-  // const { dispatch } = useContext(DarkModeContext);
-
   return (
     <div className="navbar">
       <div className="wrapper">
@@ -21,28 +17,23 @@ const Navbar = () => {
         </div>
         <div className="items">
           <div className="item">
-            <LanguageOutlinedIcon className="icon" />
+            <LanguageOutlinedIcon color="primary" />
             English
           </div>
           <div className="item">
-            <DarkModeOutlinedIcon
-              className="icon"
-              // onClick={() => dispatch({ type: "TOGGLE" })}
-            />
+            <DarkModeToggle />
           </div>
           <div className="item">
-            <FullscreenExitOutlinedIcon className="icon" />
+            <FullscreenExitOutlinedIcon color="primary" />
           </div>
           <div className="item">
-            <NotificationsNoneOutlinedIcon className="icon" />
-            <div className="counter">1</div>
+            <NotificationBadge />
           </div>
           <div className="item">
-            <ChatBubbleOutlineOutlinedIcon className="icon" />
-            <div className="counter">2</div>
+            <ChatBadge />
           </div>
           <div className="item">
-            <ListOutlinedIcon className="icon" />
+            <ListOutlinedIcon color="primary" />
           </div>
           <div className="item">
             <img
