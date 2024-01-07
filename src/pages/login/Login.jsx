9329e4847d9f348a60668197ed1b9a6c243e9685
iakev <kirimikmwongera@gmail.com>
2024-01-07 15:29:12 +0000

    Propperly named the component and added functionality to push user to home on succesful login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,74 +1,75 @@
 import React, { useState } from "react";
 import { useAPI } from "../../services/api/useAPI";
-import authenticate from "../../services/api/auth/authenticate";
+import { authenticate } from "../../services/api/auth/authenticate";
 import "./login.scss"
 
-export default function login() {
-    const [loginData, setLoginData] = useState({
-        username: "",
-        password: ""
+export default function Login() {
+  const [loginData, setLoginData] = useState({
+    username: "",
+    password: ""
+  });
+
+  const apiContext = useAPI();
+  function handleChange(event) {
+    const { name, value, type, checked } = event.target;
+    setLoginData(prevLoginData => {
+      return {
+        ...prevLoginData,
+        [name]: type === "checkbox" ? checked : value,
+      }
     });
+  }
 
-    const apiContext = useAPI();
-    function handleChange(event) {
-        const { name, value, type, checked } = event.target;
-        setLoginData(prevLoginData => {
-            return {
-                ...prevLoginData,
-                [name]: type === "checkbox" ? checked : value,
-            }
-        });
+  function validateInputs({ username, password }) {
+    if (!username) {
+      return "Username cannot be empty";
+    }
+    if (password.length < 6) {
+      return "Password should be a t least 6 characters";
+    }
+    return null;
+  }
+
+  async function handleLogin(event) {
+    event.preventDefault();
+    const error = validateInputs(loginData);
+    if (error) {
+      alert(`Error:${error}`);
+    } else {
+      await authenticate(apiContext, loginData);
+      window.location.replace('/');
     }
 
-    function validateInputs({ username, password }) {
-        if (!username) {
-            return "Username cannot be empty";
-        }
-        if (password.length < 6) {
-            return "Password should be a t least 6 characters";
-        }
-        return null;
-    }
-
-    async function handleLogin(event) {
-        event.preventDefault();
-        const error = validateInputs(loginData);
-        if (error) {
-            alert(`Error:${error}`);
-        } else {
-            await authenticate(apiContext, loginData);
-        }
-
-    }
-    return (
-        <div className="login-container">
-            <div className="form-container">
-                <h1 className="login-text">
-                    Log in to your account 🔐
-                </h1>
-                <form onSubmit={handleLogin} className="form">
-                    <div className="input-container">
-                        <input
-                            type="text"
-                            placeholder="username or email"
-                            onChange={handleChange}
-                            value={loginData.username}
-                            name="username"
-                        />
-                    </div>
-                    <div className="input-container">
-                        <input
-                            type="password"
-                            placeholder="password"
-                            onChange={handleChange}
-                            value={loginData.password}
-                            name="password"
-                        />
-                    </div>
-                    <br />
-                    <button>Login</button>
-                </form>
-            </div>
-        </div>
-    )
+  }
+  return (
+    <div className="login-container">
+      <div className="form-container">
+        <h1 className="login-text">
+          Log in to your account 🔐
+        </h1>
+        <form onSubmit={handleLogin} className="form">
+          <div className="input-container">
+            <input
+              type="text"
+              placeholder="username or email"
+              onChange={handleChange}
+              value={loginData.username}
+              name="username"
+            />
+          </div>
+          <div className="input-container">
+            <input
+              type="password"
+              placeholder="password"
+              onChange={handleChange}
+              value={loginData.password}
+              name="password"
+            />
+          </div>
+          <br />
+          <button>Login</button>
+        </form>
+      </div>
+    </div>
+  )
 }
