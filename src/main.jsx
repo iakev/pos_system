63e78459bac6493cd561e56f91008603eb7fa2ba
iakev <kirimikmwongera@gmail.com>
2024-01-07 15:10:44 +0000

    Added ThemeContextProvider that has the theme settings

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeContextProvider } from './theme/ThemeContextProvider';
 import App from './App.jsx'
 import './index.css'
 
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeContextProvider>
+      <App />
+    </ThemeContextProvider>
   </React.StrictMode>,
 )
