277a5f63e92cb705db4a699a7c81df77aafae6e9
iakev <kirimikmwongera@gmail.com>
2024-01-07 15:23:11 +0000

    Attempted to make a generic layout, this is still pending!!

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import { useAPI } from "../../services/api/useAPI";
 import Login from "../login/Login";
+import Stack from '@mui/material/Stack';
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Widget from "../../components/widget/Widget";
@@ -7,36 +8,39 @@ import Featured from "../../components/featured/Featured";
 import Chart from "../../components/chart/Chart";
 import Table from "../../components/table/Table";
 import "./home.scss";
+import { Box, Container } from "@mui/material";
 
 const Home = () => {
   const { user } = useAPI();
   return (
     <div className="home">
       {user == null ?
-        <div className="homeContainer">
+        (<Container maxWidth="sm">
           <Login />
-        </div>
+        </Container>)
         :
-        <>
-          <Sidebar />
-          <div className="homeContainer">
-            <Navbar />
-            <div className="widgets">
-              <Widget type="user" />
-              <Widget type="order" />
-              <Widget type="earning" />
-              <Widget type="balance" />
+        (<Box>
+          <Navbar />
+          <Stack>
+            <Sidebar />
+            <div className="homeContainer">
+              <div className="widgets">
+                <Widget type="user" />
+                <Widget type="order" />
+                <Widget type="earning" />
+                <Widget type="balance" />
+              </div>
+              <div className="charts">
+                <Featured />
+                <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
+              </div>
+              <div className="listContainer">
+                <div className="listTitle">Latest Transactions</div>
+                <Table />
+              </div>
             </div>
-            <div className="charts">
-              <Featured />
-              <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
-            </div>
-            <div className="listContainer">
-              <div className="listTitle">Latest Transactions</div>
-              <Table />
-            </div>
-          </div>
-        </>
+          </Stack>
+        </Box>)
       }
     </div>
   );
